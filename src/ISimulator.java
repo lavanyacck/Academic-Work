88eb8d935dc6b7f4afa3cca27f90a205bd7e267a
Lavanya <lavanya.cck@gmail.com>
2015-08-20 22:51:06 -0700

    removed files

--- src/ISimulator.java
+++
@@ -1,8 +0,0 @@
-public interface ISimulator {
-	void init();
-	Event getEvent();
-	void generate_report();
-	void schedule_event(int PID, double time, Enums.eventType type);
-	
-	public static final int num = 10000;
-}
