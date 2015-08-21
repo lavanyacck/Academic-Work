9e0e670b0f602a681e16652179e009e6bfaaecd5
Lavanya <lavanya.cck@gmail.com>
2015-08-20 22:29:05 -0700

    first commit

---
+++ src/Enums.java
@@ -0,0 +1,14 @@
+public class Enums {
+
+	enum eventType {
+		ARRIVAL, DEPARTURE, TIMESLICE, UNKNOWN
+	};
+	
+	enum CPUStatus {
+		IDLE, BUSY, UNKNOWN
+	};
+	
+	enum scheduler {
+		FCFS, SRTF, HRRN, RR
+	};
+}
